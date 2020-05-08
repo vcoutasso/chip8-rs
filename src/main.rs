a696ea8bb717a4bb9b3180b5fb223063a929ff57
Vinícius Couto <vinicouto12@gmail.com>
2020-05-08 01:31:27 -0300

    Memory implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+mod memory;
+
+use memory::Memory;
+
+#[allow(non_snake_case)]
+struct Register {
+    // Register V0
+    V0: u8,
+    // Register V1
+    V1: u8,
+    // Register V2
+    V2: u8,
+    // Register V3
+    V3: u8,
+    // Register V4
+    V4: u8,
+    // Register V5
+    V5: u8,
+    // Register V6
+    V6: u8,
+    // Register V7
+    V7: u8,
+    // Register V8
+    V8: u8,
+    // Register V9
+    V9: u8,
+    // Register VA
+    VA: u8,
+    // Register VB
+    VB: u8,
+    // Register VC
+    VC: u8,
+    // Register VD
+    VD: u8,
+    // Register VE
+    VE: u8,
+    // Should not be used by programs. Mostly used as a flag
+    VF: u8,
+    // Stores memory addresses
+    I: u16,
+    // Special purpose registers. Both DT and ST decrement by one at a rate of *dec_rate*
+    //
+    // Delay timer. Delay timer activates whenever this register is non-zero.
+    DT: u8,
+    // Sound timer. Sound timer activates whenever this register is non-zero.
+    // As long as ST has a value greater than zero, the Chip-8 buzzer will beep
+    ST: u8,
+    // Pseudo-registers below are not accessable from programs
+    //
+    // Program counter. Stores currently executing address
+    PC: u16,
+    // Stack pointer. Points to topmost level of the stack
+    SP: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Decrement rate of special purpose registers
+    const dec_rate: u8 = 60;
+
+    // The stack is used to store the address that the interpreter should return when done with a subroutine
+    // 16 bytes means up to 16 levels of nested routines
+    let mut stack: Vec<u16> = vec![0; 16];
+
+    // The memory. Notable addresses:
+    // 0x000 to 0x1FF - Reserved for the interpreter
+    // 0x200 - Start of most Chip-8 programs
+    // 0x600 - Programs intended for the ETI 660 can start at this address
+    // 0xFFF - End of Chip-8 RAM
+    let mut memory: Vec<u8> = vec![0; 4096];
+
+    let ram = Memory::new();
+
 }
---
+++ src/memory.rs
@@ -0,0 +1,56 @@
+// Struct that represents the RAM memory of the machine
+pub struct Memory {
+    ram: [u8; 4096],
+}
+
+impl Memory {
+    /// Creates a new instance of Memory with the default interpreter values
+    pub fn new() -> Memory {
+        let mut mem = Memory { ram: [0; 4096] };
+
+        mem.load_font_sprites();
+
+        mem
+    }
+
+    /// Loads the default font sprites to memory
+    /// The occupied memory address range from 0x00 to 0x80
+    fn load_font_sprites(&mut self) {
+        // Default font sprites
+        let sprites: [[u8; 5]; 16]= [
+            [0xF0, 0x90, 0x90, 0x90, 0xF0], // 0
+            [0x20, 0x60, 0x20, 0x20, 0x70], // 1
+            [0xF0, 0x10, 0xF0, 0x80, 0xF0], // 2
+            [0xF0, 0x10, 0xF0, 0x10, 0xF0], // 3
+            [0x90, 0x90, 0xF0, 0x10, 0x10], // 4
+            [0xF0, 0x80, 0xF0, 0x10, 0xF0], // 5
+            [0xF0, 0x80, 0xF0, 0x90, 0xF0], // 6
+            [0xF0, 0x10, 0x20, 0x40, 0x40], // 7
+            [0xF0, 0x90, 0xF0, 0x90, 0xF0], // 8
+            [0xF0, 0x90, 0xF0, 0x10, 0xF0], // 9
+            [0xF0, 0x90, 0xF0, 0x90, 0x90], // A
+            [0xE0, 0x90, 0xE0, 0x90, 0xE0], // B
+            [0xF0, 0x80, 0x80, 0x80, 0xF0], // C
+            [0xE0, 0x90, 0x90, 0x90, 0xE0], // D
+            [0xF0, 0x80, 0xF0, 0x80, 0xF0], // E
+            [0xF0, 0x80, 0xF0, 0x80, 0x80], // F
+        ];
+
+        // Load them to memory
+        let mut i = 0;
+        for sprite in sprites.iter() {
+            for byte in sprite.iter() {
+                self.write_byte(i, *byte);
+                i += 1;
+            }
+        }
+    }
+
+    fn write_byte(&mut self, index: usize, byte: u8) {
+        self.ram[index] = byte;
+    }
+
+    fn read_byte(&mut self, index: usize) -> u8{
+        self.ram[index]
+    }
+}
